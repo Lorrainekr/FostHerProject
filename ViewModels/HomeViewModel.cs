b7322ff09a7453b50dec8598293fdda7a76e1aa8
lorraine k <lokrinbarg@gmail.com>
2021-09-06 11:09:52 +0200

    Modification view models

---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ProjetFostHer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetFostHer.ViewModels
+{
+    public class HomeViewModel
+    {
+        public string Message { get; set; }
+        public DateTime Date { get; set; }
+        public Association Associations { get; set; }
+    }
+}
