66816a8f274d6c63df92e847b5d78c7aa7922bfe
lorraine k <lokrinbarg@gmail.com>
2021-09-06 10:19:10 +0200

    ajout classe C#

---
+++ DAL/IDal.cs
@@ -0,0 +1,15 @@
+﻿using ProjetFostHer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetFostHer.DAL
+{
+    public interface IDal : IDisposable
+    {
+        void DeleteCreateDatabase();
+        List<User> ListAllUsers();
+        void CreateUser(int id, string email, string password);
+    }
+}
