38b641edff46b99e2fb3c3202f0e66835f036497
lorraine k <lokrinbarg@gmail.com>
2021-09-09 10:57:33 +0200

    modification classe artist

--- Models/Artist.cs
+++ Models/Artist.cs
@@ -12,12 +12,25 @@ namespace ProjetFostHer.Models
         [Required(ErrorMessage = "Champ obligatoire")]
         [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Veuillez saisir une adresse mail valide")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Champ obligatoire")]
+        [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$",
+            ErrorMessage = "Votre mot de passe doit contenir au moins :" +
+                           " Une lettre majuscule" +
+                           " Une lettre minuscule" +
+                           " Un chiffre" +
+                           " Un caractère spécial" +
+                           " Une longueur minimal de huit caractères")]
         public string Password { get; set; }
         public string Address { get; set; }
+        [Required(ErrorMessage = "Champ obligatoire")]
         public string FirstName { get; set; }
+        [Required(ErrorMessage = "Champ obligatoire")]
         public string LastName { get; set; }
         public string StageName { get; set; }
+        [Required(ErrorMessage = "Champ obligatoire")]
         public string Domain { get; set; }
+        [Required(ErrorMessage = "Champ obligatoire")]
+        [StringLength(14, ErrorMessage = "Votre numéro de SIRET est composé de 14 chiffres")]
         public string Siret { get; set; }
     }
 }
