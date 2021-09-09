0044b7d6923fec3811d710cb4f4e3d70e3c3268b
lorraine k <lokrinbarg@gmail.com>
2021-09-09 12:02:27 +0200

    modification controller asso

---
+++ Controllers/AssociationController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetFostHer.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetFostHer.Controllers
+{
+    public class AssociationController : Controller
+    {
+        public IActionResult EditAssociation(int id)
+        {
+            if (id != 0)
+            {
+                using (IDal dal = new Dal())
+            }
+            return View();
+        }
+    }
+}
