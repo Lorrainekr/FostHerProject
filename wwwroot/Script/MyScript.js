1399fced8fccd12625f68fe43c70df94e1bf5aaa
lorraine k <lokrinbarg@gmail.com>
2021-09-17 16:46:34 +0200

    integration html et css js crowdfunding

---
+++ wwwroot/Script/MyScript.js
@@ -0,0 +1,30 @@
+﻿$("#project").click(function () {
+    $("#partial_project").show();
+    $("#partial_objectif").hide();
+    $("#partial_contributor").hide();
+});
+
+$("#objectif").click(function () {
+    $("#partial_project").hide();
+    $("#partial_objectif").show();
+    $("#partial_contributor").hide();
+});
+
+$("#contributions").click(function () {
+    $("#partial_project").hide();
+    $("#partial_objectif").hide();
+    $("#partial_contributor").show();
+});
+
+window.onscroll = function () { myFunction() };
+
+var subnav = document.getElementById("subnav");
+var sticky = subnav.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        subnav.classList.add("sticky")
+    } else {
+        subnav.classList.remove("sticky");
+    }
+}
