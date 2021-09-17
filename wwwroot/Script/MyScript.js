727d89dfea5ac7fd0eb172f25709f73a6169c470
lorraine k <lokrinbarg@gmail.com>
2021-09-17 21:54:42 +0200

    integration html et css js crowdfunding subnav

--- wwwroot/Script/MyScript.js
+++ wwwroot/Script/MyScript.js
@@ -1,21 +1,32 @@
 ﻿$("#project").click(function () {
     $("#partial_project").show();
     $("#partial_objectif").hide();
+    $("#partial_comment").hide();
     $("#partial_contributor").hide();
 });
 
 $("#objectif").click(function () {
     $("#partial_project").hide();
     $("#partial_objectif").show();
+    $("#partial_comment").hide();
+    $("#partial_contributor").hide();
+});
+
+$("#comment").click(function () {
+    $("#partial_project").hide();
+    $("#partial_objectif").hide();
+    $("#partial_comment").show();
     $("#partial_contributor").hide();
 });
 
 $("#contributions").click(function () {
     $("#partial_project").hide();
     $("#partial_objectif").hide();
+    $("#partial_comment").hide();
     $("#partial_contributor").show();
 });
 
+/* sticky menu scroll navbar crowdfunding*/
 window.onscroll = function () { myFunction() };
 
 var subnav = document.getElementById("subnav");
@@ -28,3 +39,17 @@ function myFunction() {
         subnav.classList.remove("sticky");
     }
 }
+
+/* sticky menu scroll contrepartie crowdfunding
+window.onscroll = function () { myFunction() };
+
+var staticcontrepartie = document.getElementById("staticcontrepartie");
+var stickycontrepartie = staticcontrepartie.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= stickycontrepartie) {
+        staticcontrepartie.classList.add("stickycontrepartie")
+    } else {
+        staticcontrepartie.classList.remove("stickycontrepartie");
+    }
+}*/
