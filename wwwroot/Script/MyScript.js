8130537ee4c2814f822f0708fd25ad9adca304fe
lorraine k <lokrinbarg@gmail.com>
2021-09-18 00:51:27 +0200

    integration html et css jpp trop crevé

--- wwwroot/Script/MyScript.js
+++ wwwroot/Script/MyScript.js
@@ -53,3 +53,18 @@ function myFunction() {
         staticcontrepartie.classList.remove("stickycontrepartie");
     }
 }*/
+/* block deroulant*/
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
